276942eb40f67c075fd94e0d6fb63e48529955bd
Neciu Dan <neciudan@Necius-MacBook-Pro.local>
2017-01-19 14:34:53 +0200

    added angular-2-mqtt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+import {
+  MqttMessage,
+  MqttModule,
+  MqttService
+}                        from 'angular2-mqtt';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +13,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,161 @@
+import {
+  ChangeDetectorRef,
+  Component,
+  Input,
+  NgModule,
+  Pipe,
+  PipeTransform
+}                        from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { FormsModule }   from '@angular/forms';
 
-import { AppComponent } from './app.component';
+import { Observable }    from 'rxjs/Observable';
+
+import {
+  MqttMessage,
+  MqttModule,
+  MqttService
+}                        from 'angular2-mqtt';
+
+@Pipe({ name: 'keys', pure: false })
+export class KeysPipe implements PipeTransform {
+  transform(map: { [key: string]: any }): string[] {
+    return Object.keys(map);
+  }
+}
+
+@Pipe({ name: 'stateToString' })
+export class StateToStringPipe implements PipeTransform {
+  private states = [
+    'CLOSED',
+    'CONNECTING',
+    'CONNECTED'
+  ];
+
+  transform(state: number): string {
+    return `${this.states[state]} (${state})`;
+  }
+}
+
+@Component({
+  selector: 'mqtt-simple',
+  template: `
+    <button type="button" class="btn btn-default" (click)="click()" [ngClass]="{'btn-danger':!msgs,'btn-success':!!msgs}">
+      <span *ngIf="!msgs">{{filter}}</span>
+      <span *ngIf="!!msgs">{{(msgs | async)?.topic}}: {{(msgs| async)?.payload.toString().substr(0,32)}}</span>
+    </button>
+  `
+})
+export class MqttSimpleComponent {
+  @Input() public filter: string;
+  public msgs: Observable<MqttMessage> = null;
+
+  constructor(private mqttService: MqttService, private cdRef: ChangeDetectorRef) { }
+
+  public click() {
+    if (!this.msgs) {
+      this.msgs = this.mqttService.observe(this.filter);
+    } else {
+      this.msgs = null;
+    }
+    this.cdRef.detectChanges();
+  }
+}
+
+@Component({
+  selector: 'mqtt-connection',
+  template: `
+    <h2>Connection</h2>
+    <table class="table">
+      <tr>
+        <th>Connection</th>
+        <td>{{(mqtt.state | async) | stateToString}}</td>
+      </tr>
+    </table>
+
+    <h2>Observers</h2>
+    <mqtt-simple filter="#"></mqtt-simple>
+    <mqtt-simple filter="$SYS/#"></mqtt-simple>
+
+    <h2>Subscriptions</h2>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Filter</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr *ngFor="let filter of mqtt.observables | keys">
+          <td>{{filter}}</td>
+        </tr>
+      </tbody>
+    </table>
+
+    <form name="publishForm">
+      <div class="form-group">
+        <label for="topic">Topic</label>
+        <input type="text" class="form-control" id="topic" name="topic" [(ngModel)]="topic">
+      </div>
+      <div class="form-group">
+        <label for="message">Message</label>
+        <textarea class="form-control" id="message" name="message" [(ngModel)]="message"></textarea>
+      </div>
+      <button type="submit" class="btn btn-default" (click)="publish(topic, message)">Publish</button>
+    </form>
+  `
+})
+export class MqttConnectComponent {
+  public topic: string;
+  public message: string;
+  constructor(public mqtt: MqttService) { }
+
+  public publish(topic: string, message: string) {
+    this.mqtt.publish(topic, message, {qos: 1}).subscribe((err)=>{
+      console.log(err);
+    });
+  }
+}
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <mqtt-connection></mqtt-connection>
+  `
+})
+export class ExampleComponent {
+  constructor(private mqttService: MqttService) { }
+}
+
+export const MQTT_SERVICE_OPTIONS = {
+  hostname: 'localhost',
+  port: 9001,
+  path: '/mqtt'
+};
+
+export function mqttServiceFactory() {
+  return new MqttService(MQTT_SERVICE_OPTIONS);
+}
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    MqttModule.forRoot({
+      provide: MqttService,
+      useFactory: mqttServiceFactory
+    })
+  ],
+  declarations: [
+    ExampleComponent,
+    MqttSimpleComponent,
+    MqttConnectComponent,
+    KeysPipe,
+    StateToStringPipe
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    ExampleComponent
+  ]
 })
+
 export class AppModule { }
